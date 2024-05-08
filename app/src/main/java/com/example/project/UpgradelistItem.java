c7d9c16e5c7935dd6828ef90ae35be9dd5806b13
a20artje <coolvids.bruh@gmail.com>
2024-05-08 13:37:58 +0200

    Added Java Class representing upgrade list item

---
+++ app/src/main/java/com/example/project/UpgradelistItem.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+@SuppressWarnings("WeakerAccess")
+public class UpgradelistItem {
+
+    private String title;
+
+    public UpgradelistItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
