035e0d9f404d9b5f31aa9edfc434db1ae078ef75
a20artje <coolvids.bruh@gmail.com>
2024-05-09 11:24:07 +0200

    Removed old toolbar and added new with image button about

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         upgrades = new ArrayList<>();
 
