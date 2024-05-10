512df40c1b631830002845e107a88fd2e0ad06d4
a20artje <coolvids.bruh@gmail.com>
2024-05-10 12:04:12 +0200

    created details view

---
+++ app/src/main/java/com/example/project/UpgradeDetails.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class UpgradeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upgrade_details);
+
+    }
+}
