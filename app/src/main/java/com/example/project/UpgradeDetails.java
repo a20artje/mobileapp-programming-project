290bac846a8ad8969edbb88f08c4320a68d1b823
a20artje <coolvids.bruh@gmail.com>
2024-05-22 12:38:32 +0200

    Added purchasing, as a shared preference

--- app/src/main/java/com/example/project/UpgradeDetails.java
+++ app/src/main/java/com/example/project/UpgradeDetails.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +17,16 @@ public class UpgradeDetails extends AppCompatActivity {
     private int cost;
     private String description;
 
+    SharedPreferences cookiesPreferenceRef;
+    SharedPreferences.Editor cookiesPreferenceEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade_details);
+
+        cookiesPreferenceRef = getSharedPreferences("cookieStats", MODE_PRIVATE);
+        cookiesPreferenceEditor = cookiesPreferenceRef.edit();
 
         Bundle extras = getIntent().getExtras();
 
@@ -26,21 +35,43 @@ public class UpgradeDetails extends AppCompatActivity {
             cookiesPerSecond = extras.getFloat("cookiesPerSecond");
             cost = extras.getInt("cost");
             description = extras.getString("description");
+            int amountOfTimesUpgraded = cookiesPreferenceRef.getInt(name, 0);
+            String amountString = "You own: " + String.valueOf(amountOfTimesUpgraded) + " " + name;
 
             TextView nameText = findViewById(R.id.upgrade_name);
             TextView cpsText = findViewById(R.id.upgrade_cps);
             TextView costText = findViewById(R.id.upgrade_cost);
             TextView descriptionText = findViewById(R.id.upgrade_description);
+            TextView amountText = findViewById(R.id.upgrade_amount);
 
             nameText.setText(name);
             cpsText.setText(String.valueOf(cookiesPerSecond));
             costText.setText(String.valueOf(cost));
             descriptionText.setText(description);
+            amountText.setText(amountString);
         }
+
 
     }
 
     public void closeActivity(View view) {
         finish();
+    }
+
+    public void purchase(View view) {
+
+        View purchaseButton = findViewById(R.id.purchase_button_card);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_down);
+        purchaseButton.startAnimation(animation);
+
+        int amountOfTimesUpgraded = cookiesPreferenceRef.getInt(name, 0);
+        amountOfTimesUpgraded++;
+        cookiesPreferenceEditor.putInt(name, amountOfTimesUpgraded);
+        cookiesPreferenceEditor.apply();
+
+        String amountString = "You own: " + String.valueOf(amountOfTimesUpgraded) + " " + name;
+        TextView amountText = findViewById(R.id.upgrade_amount);
+        amountText.setText(amountString);
+
     }
 }
